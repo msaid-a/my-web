d297ede44409e6991176e1a70651621332e9c6d1
No Connection <muhammad.arrafi@fast-8.com>
2021-04-06 16:48:02 +0700

    build

---
+++ build/precache-manifest.9e05915d1549f37de881507e5a1cbf2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31700281dabdd83fb655268a4f5a4a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4651007ea007c344a7fc",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4651007ea007c344a7fc",
+    "url": "/static/js/2.b3bb3aa2.chunk.js"
+  },
+  {
+    "revision": "0b549b1f8a3c3e9f44c2",
+    "url": "/static/js/main.8e87a167.chunk.js"
+  },
+  {
+    "revision": "13b4f9284216e6d082db",
+    "url": "/static/js/runtime-main.2ed82ed8.js"
+  }
+]);
